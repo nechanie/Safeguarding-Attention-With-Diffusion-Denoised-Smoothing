935688c6987f1ea9873ad545d8e290ae7c007688
mattmorgan6 <morgamat@oregonstate.edu>
2023-05-14 12:35:35 -0700

    Add install dataset script

---
+++ ResNet-50-CBAM-PyTorch/install_dataset.sh
@@ -0,0 +1,4 @@
+mkdir -p datasets
+cd datasets
+wget "https://drive.google.com/uc?export=download&id=1FM7onb0IOkByP-oHRpyVuFWOyqJCVCNW&confirm=yes" -O "intel_dataset.tar.gz"
+tar -xzf "intel_dataset.tar.gz"
