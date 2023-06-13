645c09ce62d5d621ee9d3ade9e11ac976b3301b3
mattmorgan6 <morgamat@oregonstate.edu>
2023-06-12 21:23:46 -0700

    Add PGD for cifar

---
+++ ResNet-50-CBAM-PyTorch/PGD.py
@@ -0,0 +1,90 @@
+import json
+import time
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
+from tqdm import tqdm
+import os
+import torchvision.transforms.functional as F
+
+import load_dataset as DatasetLoader
+from runtime_args import args
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+device = torch.device("cuda:0" if torch.cuda.is_available() and args.device == 'gpu' else 'cpu')
+DEBUG = args.DEBUG
+
+
+def pgd(input, labels, model, iters, epsilon, stepsize, loss = None):
+    input_copy = input.detach().clone() # Clones to create new set of adversarial images
+
+    if(loss == None):
+        loss = nn.CrossEntropyLoss()
+
+    for _ in range(iters):
+        input_copy.requires_grad = True
+        model.zero_grad()
+        _, pred = model(input_copy)
+        loss_obj = loss(pred, labels)
+        loss_obj.backward()                      
+        grad = input_copy.grad.detach()
+        grad = grad.sign()
+        input_copy = input_copy + stepsize * grad
+
+        # Project x_copy onto x to get our adversarial x
+        input_copy = input + torch.clamp(input_copy - input, min=-epsilon, max=epsilon)
+        input_copy = input_copy.detach()
+        input_copy = torch.clamp(input_copy, min=0, max=1)
+
+    return input_copy
+
+
+
+# TODO: test this func thoroughly
+def generate_adversarial_images(count, model, dataset, niter, epsilon, stepsize, save_path, randinit = False):
+    counts = [0 for _ in range(1000)]
+    class_map = json.load(open('cifar_classes.json', 'r'))
+    loss = nn.CrossEntropyLoss().to(device)
+    cleanImages = []
+    advImages = []
+    cleanLabels = []
+    for i, entry in tqdm(enumerate(dataset)):
+        inputs = entry['image'].to(device)
+        labels = entry['label'].to(device)
+               
+        # cleanImages.append(inputs)
+        # advImages.append(pgd(inputs, labels, model, niter, epsilon, stepsize, loss))
+        # cleanLabels.append(labels)
+
+        save_all_adversarial_images(save_path, pgd(inputs, labels, model, niter, epsilon, stepsize, loss), labels, class_map, counts)
+
+    # return torch.cat(cleanImages), torch.cat(advImages), torch.cat(cleanLabels) #Concatenate all adv images into 1 4-d tensor instead of a list of smaller 4-d tensors with 1 block of data in them
+
+
+
+
+def show(imgs):
+    if not isinstance(imgs, list):
+        imgs = [imgs]
+    fig, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        img = img.detach()
+        img = F.to_pil_image(img)
+        axs[0, i].imshow(np.asarray(img))
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+    fig.show()
+
+
+def save_all_adversarial_images(dirname, images, labels, class_map, counts):
+    
+    for idx in range(len(images)):
+        label = labels[idx]
+        new_label = class_map[str(label.item())]
+        filename = dirname + "/" + new_label + "/" + str(counts[label.item()]) + ".png"
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        save_image(images[idx], filename)
+        counts[label] = counts[label] + 1
---
+++ ResNet-50-CBAM-PyTorch/generate_PGD_and_validate.py
@@ -0,0 +1,62 @@
+import json
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
+from tqdm import tqdm
+import os
+import torchvision.transforms.functional as F
+import timm
+
+from valid import validate
+import load_dataset as DatasetLoader
+from runtime_args import args
+from PGD import generate_adversarial_images, save_all_adversarial_images
+
+    
+def save_unnormalized_img(img, filename, data_config):
+    inverse_transform = transforms.Normalize(mean=[-m/s for m, s in zip(data_config['mean'], data_config['std'])], std=[1/s for s in data_config['std']])
+    inversed_image = inverse_transform(img)
+    save_image(inversed_image, filename)
+
+
+device = torch.device("cuda" if torch.cuda.is_available() and args.device == 'gpu' else 'cpu')
+LOCAL_MODEL_PATH = "./pretrained_coatnet.pt"
+local_model_exists = os.path.exists(LOCAL_MODEL_PATH)
+
+
+if __name__ == "__main__":
+    print(f"PyTorch device: {device}", flush=True)
+
+    path_to_model = args.pretrained_path or LOCAL_MODEL_PATH
+    assert path_to_model[-3:] == '.pt'
+
+    print(path_to_model, flush=True)
+
+    DATASET_SIZE = 0.2
+
+    # Dataset Creation
+    dataset = DatasetLoader.LoadDataset(dataset_folder_path=args.data_folder, image_depth=args.img_depth,
+                                transform=transforms.ToTensor(), train=False, validate=False)
+
+    sampler = DatasetLoader.get_subset_random_sampler(dataset, DATASET_SIZE)
+
+    test_generator = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers,
+                                    pin_memory=True, sampler=sampler)
+    
+    model = torch.load(path_to_model)
+    model = model.to(device)
+    model.eval()
+
+    save_path = f"{args.PGD_save_path}/e_{args.PGD_epsilon}_n_{args.PGD_niter}_s_{args.PGD_stepsize/255}"
+    
+    print(f"\n== Generating Adversarial Images with epsilon {args.PGD_epsilon}\n")
+    generate_adversarial_images(args.PGD_image_count, model, test_generator, args.PGD_niter, args.PGD_epsilon, args.PGD_stepsize/255, save_path)
+    
+    # print("\n== Saving Images to drive\n")
+    # save_all_adversarial_images(f"{args.PGD_save_path}/e_{args.PGD_epsilon}_n_{args.PGD_niter}_s_{args.PGD_stepsize/255}", advImages, labels)
+
+    print("done!")
+
+    #validate(model, advImages)
